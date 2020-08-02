23537d483068ad99dc2c8fcfadb1f74d00716a3d
Heesun Lee <csheesun@gmail.com>
2020-08-02 19:08:37 +0000

    upload .net tutorial-web initially

---
+++ myApp/Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("The current time is " + DateTime.Now);
+        }
+    }
+}
+
+
